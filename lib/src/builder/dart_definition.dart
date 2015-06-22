9b832debe623ecae15591baee3d8bdec061fd058
Matan Lurey <matan@lurey.org>
2015-06-21 22:11:15 -0700

    Kill builders (moved to dart_builder), up pubspec.

--- lib/src/builder/dart_definition.dart
+++
@@ -1,9 +0,0 @@
-library generator.src.builder.dart_definition;
-
-import 'package:generator/src/builder/dart_source.dart';
-
-abstract class DartDefinition extends DartSource {
-  final String name;
-
-  DartDefinition(this.name);
-}
